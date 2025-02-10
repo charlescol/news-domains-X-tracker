30ec353825ebe7633f5bd2307b79297e04260d16
charlescol <charlescol@hotmail.fr>
2025-02-10 21:49:59 +0100

    chore: enable daily top 100 job

--- src/news_tracker_run.py
+++ src/news_tracker_run.py
@@ -12,7 +12,7 @@ def run():
     """
     Process accounts in batches of 100 per token, record the state for future executions. 
     """
-    tokens = [os.getenv("X_BEARER_TOKEN_1"), os.getenv("X_BEARER_TOKEN_2"), os.getenv("X_BEARER_TOKEN_3"), os.getenv("X_BEARER_TOKEN_4")]
+    tokens = [os.getenv("X_BEARER_TOKEN_1"), os.getenv("X_BEARER_TOKEN_2"), os.getenv("X_BEARER_TOKEN_3")]
     for token in tokens:
         usernames = extractUsernames(useProgressFile=True)
         response = fetchXStats(",".join(usernames), token)
